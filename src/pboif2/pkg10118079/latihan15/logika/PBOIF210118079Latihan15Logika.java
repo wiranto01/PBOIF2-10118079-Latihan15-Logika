35a89a3db9626dd059bdaefd7bfff29e90beacee
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:35:07 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan15/logika/PBOIF210118079Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan15.logika;
+
+/**
+ *
+ * @author
+  * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a && b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b = " + !(a && b);
+    }
+        
+}
